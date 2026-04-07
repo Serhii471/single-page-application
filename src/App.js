ee3dc2d010550f90a1b85640a296a054e66e78c7
Serhii471 <sergijpron250@gmail.com>
2026-04-07 15:17:40 +0300

    Upd-1

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-    <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className={`app-container ${theme}`}>
         <Header toggleTheme={toggleTheme} theme={theme} />
         <main className="main-content">
